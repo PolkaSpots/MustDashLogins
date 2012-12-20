399c80278cf0592c50bde5530f17fc613ef0338c
Simon Morley <simon@polkaspots.com>
2012-12-20 20:50:00 +0000

    Added CSS and Template

--- mustache.js
+++ mustache.js
@@ -783,7 +783,7 @@ $('head').append( '<style>' + ps_custom_css +'</style>' );
 
 // General Stuff //
 
-$('<div id="footer-left">Copyright &#169; 2012 PolkaSpots Limited.</div>').insertAfter('#container');
+$('<div id="footer"><div id="footer-left">Copyright &#169; 2012 PolkaSpots Limited.</div><div id="footer-right"><a href="http://polkaspots.com/privacy-policy/" target="_self" title="Privacy Policy">  Privacy Policy :</a><a href="http://polkaspots.com/terms/" target="_self" title="Terms of Use">Terms of Use : </a><a href="http://polkaspots.com/support">Help : </a><a href="http://polkaspots.com/powered-by-polkaspots">Powered by PolkaSpots</a></div></div>').insertAfter('#container');
 
 $(1 == 1) ? polkaSMS(loc) : '';
 $(1 == 1) ? polkaLogin(loc) : '';
